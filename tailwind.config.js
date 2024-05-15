f6421368dc892096046617d340fbc3fb26b09736
Clara Ricioni <bergamoclara9@gmail.com>
2024-05-15 17:26:11 -0300

    fix: tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,18 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
-        inter:["Inter",'sans-serif'],
-        garamond:["EB Garamond", 'sans-serif'],
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        garamond: ["EB Garamond", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"]
       },
-      colors:{
-        BackgroundAboutUs: '#F0F1EE',
+      colors: {
+        BackgroundAboutUs: "#F0F1EE",
         moss: "#354733",
-      },
-        raleway: ["Raleway", 'sans-serif']
-      },
-      colors:{
         primaryLunarGreen: "#1A3306",
-        primaryAvacado: "#8A9B6E",
-        
+        primaryAvacado: "#8A9B6E"
       }
-    },
+    }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
