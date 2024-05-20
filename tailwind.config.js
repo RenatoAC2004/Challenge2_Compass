bbfb659002c35044ceed15db9d351ae04a686f79
RenatoAC2004 <108144847+RenatoAC2004@users.noreply.github.com>
2024-05-20 00:12:12 -0300

    feat: Implemented responsive PDP
    Also added screensizes to tailwind config file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,25 +8,34 @@ module.exports = {
         garamond: ["EB Garamond", "sans-serif"],
         raleway: ["Raleway", "sans-serif"],
         pacifico: ["Pacifico", "sans-serif"],
-        lato: ["Lato", "sans-serif"]
+        lato: ["Lato", "sans-serif"],
       },
       colors: {
         moss: "#354733",
         primaryLunarGreen: "#1A3306",
         primaryAvacado: "#8A9B6E",
-        borderColor: '#DDDEA0',
+        borderColor: "#DDDEA0",
         textSecondary: "#808276",
         inputBorders: "#AFB2AA",
         mainBackground: "#F0F1EE",
         title: "#1E1E24",
         cardBackground: "#F8F9FB",
         greenLime: "#83E281",
-        greenCactus: "#158212"
+        greenCactus: "#158212",
       },
       backgroundImage: {
-        'footer-texture': "url('./src/assets/imgFundo.png')"
+        "footer-texture": "url('./src/assets/imgFundo.png')",
       },
-    }
+    },
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      xxl: "1700px",
+    },
   },
-  plugins: []
-};
+  plugins: [],
+}
